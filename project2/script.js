02f90396ece6029ce97306d66a7b0db99ccb4160
notconnie <114441193+notconnie@users.noreply.github.com>
2022-12-01 18:11:38 -0800

    figured out header, bkgd, topnav

--- project2/script.js
+++ project2/script.js
@@ -1,4 +1,14 @@
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "main-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "main-nav";
+    }
+  }
+
+
 
 'use strict'
 
